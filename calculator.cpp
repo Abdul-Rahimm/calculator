ca7c1ac0b96150eaf1a6ee2576017cd7d9585e4b
Abdul-Rahimm <k213951@nu.edu.pk>
2025-08-10 17:19:49 +0500

    I have added add()

---
+++ calculator.cpp
@@ -0,0 +1,20 @@
+// بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+#include <bits/stdc++.h>
+using namespace std;
+#define all(x) x.begin(), x.end()
+typedef vector<int> vi;
+typedef vector<vi> vii;
+
+int add(int x, int y)
+{
+    return x + y;
+}
+
+// subs and mul --> RT
+
+int main()
+{
+    cout << "Welcome to the Calculator!" << endl;
+
+    cout << add(2, 5) << endl;
+}
